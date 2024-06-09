510ee61586b61e0f71a87108cd7bc8ee8d255697
ltc01 <laxmichoudhary909@gmail.com>
2024-06-09 12:12:13 +0530

    Done with 1solution

---
+++ leetCodeDay_2.js
@@ -0,0 +1,23 @@
+// var maximumWealth = function(accounts) {
+//     var richCustomer = 0;
+//     for(let i=0;i<accounts.length;i++){
+//         var cusWealth = 0;
+//         for(let j=0;j<accounts[i].length;j++){
+//             cusWealth += accounts[i][j]
+//         }
+//         richCustomer = Math.max(richCustomer, cusWealth)
+
+//     }
+//     console.log(richCustomer)
+// };
+
+// maximumWealth([[1, 2,3 ], [3, 2, 1]]);
+// maximumWealth([[1,5],[7,3],[3,9],[1,3]])
+
+
+var fizzBuzz = function(n) {
+    
+};
+
+fizzBuzz(3)
+// fizzBuzz(15)
