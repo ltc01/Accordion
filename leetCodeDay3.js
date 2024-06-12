94ff981f3fe9a10c3390c1bcb1ff81592698caf8
ltc01 <laxmichoudhary909@gmail.com>
2024-06-12 16:26:53 +0530

    solution

---
+++ leetCodeDay3.js
@@ -0,0 +1,13 @@
+// --------1122. Relative Sort Array-------------
+
+var relativeSortArray = function(arr1, arr2) {
+    const orderMap = new Map(arr2.map((item, index) => [item, index]));
+    return arr1.sort((a, b) => 
+      orderMap.has(a) && orderMap.has(b) ? orderMap.get(a) - orderMap.get(b) : 
+      orderMap.has(a) ? -1 : 
+      orderMap.has(b) ? 1 : 
+      a - b
+    );
+};
+
+console.log(relativeSortArray([2,3,1,3,2,4,6,7,9,2,19], [2,1,4,3,9,6]))
