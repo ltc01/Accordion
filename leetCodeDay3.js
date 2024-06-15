ceaf53289a73de193aae97622e3c1e6718af3811
ltc01 <laxmichoudhary909@gmail.com>
2024-06-15 19:43:47 +0530

    3rd solution of the day

--- leetCodeDay3.js
+++ leetCodeDay3.js
@@ -213,3 +213,21 @@
 
 // isValid("()");
 // isValid("()[]{}");
+
+// -------------26. Remove Duplicates from Sorted Array-----------
+
+var removeDuplicates = function(nums) {
+    if (nums.length === 0 ) return 0;
+    let j = 1;
+
+    for (let i= 1; i<nums.length; i++){
+        if(nums[i] !== nums[i-1]){
+            console.log(nums[i], nums[i-1])
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+    console.log(j, nums.slice(0,j))
+};
+
+removeDuplicates([10,0,1,1,1,2,2,3,3,4])
